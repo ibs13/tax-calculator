4bc577efa2d6f99aba8a33b55b45b0a0b30fe3b3
ibs13 <ibm.sabbir@gmail.com>
2025-04-29 19:36:10 +0200

    Setup project using react, typescript and tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <>
-      <h1>Danish Tax Calculator</h1>
+      <h1 className="text-5xl text-black-700 font-extrabold text-center mt-3">
+        Danish Tax Calculator
+      </h1>
     </>
   );
 };
